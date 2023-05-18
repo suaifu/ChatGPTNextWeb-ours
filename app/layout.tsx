8cc184f43ed9709d35e7a4e6a459bc9ba1822124
Kwokfu <g_uo_shuai@163.com>
2023-05-18 19:35:22 +0800

    调整

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,6 +42,7 @@ export default function RootLayout({
           rel="stylesheet"
         ></link>
         <script src="/serviceWorkerRegister.js" defer></script>
+        <scirpt src="/baidustatistics.js" defer></scirpt>
         <script
           async
           src="https://umami.guoshuaifu.cn/script.js"
