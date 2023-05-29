2195d6ba90af370af0b5e79b1c78d88044ac010b
Kwokfu <g_uo_shuai@163.com>
2023-05-29 21:59:37 +0800

    调整key轮询，提升查询效率

--- app/config/server.ts
+++ app/config/server.ts
@@ -34,8 +34,12 @@ export const getServerSideConfig = () => {
     );
   }
 
+  //从这至开始-Key池轮询
+  const apiKeys = (process.env.OPENAI_API_KEY ?? "").split(",");
+  const apiKey = apiKeys.at(Math.floor(Math.random() * apiKeys.length)) ?? "";
+
   return {
-    apiKey: process.env.OPENAI_API_KEY,
+    apiKey, //这里进行调用
     code: process.env.CODE,
     codes: ACCESS_CODES,
     needCode: ACCESS_CODES.size > 0,
