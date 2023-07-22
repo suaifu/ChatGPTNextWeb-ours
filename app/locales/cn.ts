373d54291db397bfb0e6894990925d83a5b98936
Kwokfu <g_uo_shuai@163.com>
2023-07-22 17:59:59 +0800

    chatgpt接入siri

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -1,4 +1,4 @@
-import {SubmitKey} from "../store/config";
+import { SubmitKey } from "../store/config";
 
 const cn = {
   WIP: "该功能仍在开发中……",
@@ -9,6 +9,7 @@ const cn = {
     **回答：** 当然可以，还可以在这里放一些问题解答，太棒啦！
     常见问题与使用说明点击查看 **[使用方法](https://www.guoshuaifu.cn/index.php/chatgpt-use.html)** 
     提问技巧与套路点击查看 **[ChatGPT快捷指令](https://kjgpt.talkai.icu)**
+    接入Siri的方法点击查看 **[ChatGPT接入Siri](https://www.guoshuaifu.cn/index.php/archives/326/)**
     卡顿或无响应？可能是用户量太大， [添加微信获得独立账号 KEY 使用](/gptwx.jpg)
     欢迎来聊聊  [QQ群:160798639](http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=YuWgTQrri80UNwaYa2GEkZ850c62__oH&authKey=zNaA7KWQjlW7pbJuDMc45l6i3Cgp6fXpgUcdkYEmItQlZBrHJhMPlS2TxAcmfcDX&noverify=0&group_code=160798639)`,
   },
@@ -261,6 +262,7 @@ const cn = {
      **回答：** 当然可以，很高兴给您打电话。我今天想向您介绍我们公司的最新产......，太棒啦！
      常见问题与使用说明点击查看 **[使用方法](https://www.guoshuaifu.cn/index.php/chatgpt-use.html)** 
      提问技巧与套路点击查看 **[ChatGPT快捷指令](https://kjgpt.talkai.icu)**
+     接入Siri的方法点击查看 **[ChatGPT接入Siri](https://www.guoshuaifu.cn/index.php/archives/326/)**
      卡顿或无响应？可能是用户量太大， **[添加微信makerkwok获得独立账号 KEY 使用](/gptwx.jpg)**
      更新了新模型 gpt-3.5-turbo-16k,回答更智能，期待你的体验
      欢迎来聊聊  [QQ群:160798639](http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=YuWgTQrri80UNwaYa2GEkZ850c62__oH&authKey=zNaA7KWQjlW7pbJuDMc45l6i3Cgp6fXpgUcdkYEmItQlZBrHJhMPlS2TxAcmfcDX&noverify=0&group_code=160798639)`,
@@ -354,10 +356,10 @@ const cn = {
 };
 
 type DeepPartial<T> = T extends object
-    ? {
+  ? {
       [P in keyof T]?: DeepPartial<T[P]>;
     }
-    : T;
+  : T;
 
 export type LocaleType = typeof cn;
 export type PartialLocaleType = DeepPartial<typeof cn>;
